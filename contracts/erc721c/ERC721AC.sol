8307920342b77d3554b79f674bac141b6c35115f
Nathan Gang <nathan.gang@limitbreak.com>
2023-04-25 12:13:00 -0600

    Add ERC-721A variants of ERC-721C and ERC-721CW tokens

---
+++ contracts/erc721c/ERC721AC.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "../utils/CreatorTokenBase.sol";
+import "erc721a/contracts/ERC721A.sol";
+
+/**
+ * @title ERC721AC
+ * @author Limit Break, Inc.
+ * @notice 
+ */
+abstract contract ERC721AC is ERC721A, CreatorTokenBase {
+
+    constructor(address transferValidator_, string memory name_, string memory symbol_) 
+    CreatorTokenBase(transferValidator_)
+    ERC721A(name_, symbol_) {
+        setTransferValidator(transferValidator_);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(ICreatorToken).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    /// @dev Ties the erc721a _beforeTokenTransfers hook to more granular transfer validation logic
+    function _beforeTokenTransfers(
+        address from,
+        address to,
+        uint256 startTokenId,
+        uint256 quantity
+    ) internal virtual override {
+        for (uint256 i = 0; i < quantity;) {
+            _validateBeforeTransfer(from, to, startTokenId + i);
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /// @dev Ties the open-zeppelin _afterTokenTransfer hook to more granular transfer validation logic
+    function _afterTokenTransfers(
+        address from,
+        address to,
+        uint256 startTokenId,
+        uint256 quantity
+    ) internal virtual override {
+        for (uint256 i = 0; i < quantity;) {
+            _validateAfterTransfer(from, to, startTokenId + i);
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function _msgSenderERC721A() internal view virtual override returns (address) {
+        return _msgSender();
+    }
+}
