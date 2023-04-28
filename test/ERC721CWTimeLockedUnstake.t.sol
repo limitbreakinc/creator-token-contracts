be75443b84d161e50122bbde138a0cf9413c190e
Nathan Gang <nathan.gang@limitbreak.com>
2023-04-28 15:09:19 -0600

    Add tests for time locked erc721c preset

---
+++ test/ERC721CWTimeLockedUnstake.t.sol
@@ -0,0 +1,284 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "./mocks/ERC721Mock.sol";
+import "./mocks/ERC721CWTimeLockedUnstakeMock.sol";
+import "./CreatorTokenTransferValidator.t.sol";
+
+contract ERC721CWTimeLockedUnstakeTest is CreatorTokenTransferValidatorTest {
+
+    ERC721Mock public wrappedTokenMock;
+    ERC721CWTimeLockedUnstakeMock public tokenMock;
+
+    function setUp() public virtual override {
+        super.setUp();
+        
+        wrappedTokenMock = new ERC721Mock();
+        tokenMock = new ERC721CWTimeLockedUnstakeMock(1 days, address(wrappedTokenMock));
+        tokenMock.setToCustomSecurityPolicy(address(validator), TransferSecurityLevels.One, 1, 0);
+    }
+
+    function _deployNewToken(address creator) internal virtual override returns (ITestCreatorToken) {
+        vm.startPrank(creator);
+        address wrappedToken = address(new ERC721Mock());
+        ITestCreatorToken token = ITestCreatorToken(address(new ERC721CWTimeLockedUnstakeMock(1 days, wrappedToken)));
+        vm.stopPrank();
+        return token;
+    }
+
+    function _mintToken(address tokenAddress, address to, uint256 tokenId) internal virtual override {
+        address wrappedTokenAddress = ERC721CWTimeLockedUnstakeMock(tokenAddress).getWrappedCollectionAddress();
+        vm.startPrank(to);
+        ERC721Mock(wrappedTokenAddress).mint(to, tokenId);
+        ERC721Mock(wrappedTokenAddress).setApprovalForAll(tokenAddress, true);
+        ERC721CWTimeLockedUnstakeMock(tokenAddress).mint(to, tokenId);
+        vm.stopPrank();
+    }
+
+    function testSupportedTokenInterfaces() public {
+        assertEq(tokenMock.supportsInterface(type(ICreatorToken).interfaceId), true);
+        assertEq(tokenMock.supportsInterface(type(IERC721).interfaceId), true);
+        assertEq(tokenMock.supportsInterface(type(IERC721Metadata).interfaceId), true);
+        assertEq(tokenMock.supportsInterface(type(IERC165).interfaceId), true);
+    }
+
+    function testCanUnstakeReturnsFalseWhenTokensDoNotExist(uint256 tokenId) public {
+        assertFalse(tokenMock.canUnstake(tokenId));
+    }
+
+    function testCanUnstakeReturnsFalseForStakedTokenIdsBeforeTimelockExpiration(address to, uint256 tokenId, uint256 secondsBeforeExpiration) public {
+        vm.assume(to != address(0));
+        _mintToken(address(tokenMock), to, tokenId);
+
+        uint256 expirationTimestamp = tokenMock.getStakedTimestamp(tokenId) + tokenMock.getTimelockSeconds();
+        vm.assume(secondsBeforeExpiration < expirationTimestamp && secondsBeforeExpiration > 1);
+        vm.warp(expirationTimestamp - secondsBeforeExpiration);
+        assertFalse(tokenMock.canUnstake(tokenId));
+    }
+
+    function testCanUnstakeReturnsTrueForStakedTokenIdsAfterTimelockExpiration(address to, uint256 tokenId, uint256 secondsPastExpiration) public {
+        vm.assume(to != address(0));
+        _mintToken(address(tokenMock), to, tokenId);
+
+        uint256 expirationTimestamp = tokenMock.getStakedTimestamp(tokenId) + tokenMock.getTimelockSeconds();
+        uint256 maxSecondsPast = type(uint256).max - expirationTimestamp;
+        vm.assume(secondsPastExpiration < maxSecondsPast);
+        vm.warp(expirationTimestamp + secondsPastExpiration);
+        assertTrue(tokenMock.canUnstake(tokenId));
+    }
+
+    function testWrappedCollectionHoldersCanStakeTokens(address to, uint256 tokenId) public {
+        vm.assume(to != address(0));
+        vm.assume(to != address(tokenMock));
+
+        vm.startPrank(to);
+        wrappedTokenMock.mint(to, tokenId);
+        wrappedTokenMock.setApprovalForAll(address(tokenMock), true);
+        tokenMock.stake(tokenId);
+        vm.stopPrank();
+
+        assertEq(tokenMock.ownerOf(tokenId), to);
+        assertEq(wrappedTokenMock.ownerOf(tokenId), address(tokenMock));
+    }
+
+    function testRevertsWhenNativeFundsIncludedInStake(address to, uint256 tokenId, uint256 value) public {
+        vm.assume(to != address(0));
+        vm.assume(to != address(tokenMock));
+        vm.assume(value > 0);
+
+        vm.startPrank(to);
+        wrappedTokenMock.mint(to, tokenId);
+        wrappedTokenMock.setApprovalForAll(address(tokenMock), true);
+        vm.deal(to, value);
+        vm.expectRevert(ERC721CW.ERC721CW__DefaultImplementationOfStakeDoesNotAcceptPayment.selector);
+        tokenMock.stake{value: value}(tokenId);
+        vm.stopPrank();
+    }
+
+    function testRevertsWhenUnauthorizedUserAttemptsToStake(address to, address unauthorizedUser, uint256 tokenId) public {
+        vm.assume(to != address(0));
+        vm.assume(unauthorizedUser != address(0));
+        vm.assume(to != unauthorizedUser);
+        vm.assume(to != address(tokenMock));
+
+        vm.startPrank(to);
+        wrappedTokenMock.mint(to, tokenId);
+        wrappedTokenMock.setApprovalForAll(address(tokenMock), true);
+        vm.stopPrank();
+
+        vm.startPrank(unauthorizedUser);
+        vm.expectRevert(ERC721CW.ERC721CW__CallerNotOwnerOfWrappedToken.selector);
+        tokenMock.stake(tokenId);
+        vm.stopPrank();
+    }
+
+    function testRevertsWhenApprovedOperatorAttemptsToStake(address to, address approvedOperator, uint256 tokenId) public {
+        vm.assume(to != address(0));
+        vm.assume(approvedOperator != address(0));
+        vm.assume(to != approvedOperator);
+        vm.assume(to != address(tokenMock));
+
+        vm.startPrank(to);
+        wrappedTokenMock.mint(to, tokenId);
+        wrappedTokenMock.setApprovalForAll(address(tokenMock), true);
+        wrappedTokenMock.setApprovalForAll(approvedOperator, true);
+        vm.stopPrank();
+
+        vm.startPrank(approvedOperator);
+        vm.expectRevert(ERC721CW.ERC721CW__CallerNotOwnerOfWrappedToken.selector);
+        tokenMock.stake(tokenId);
+        vm.stopPrank();
+    }
+
+    function testRevertsWhenUnauthorizedUserAttemptsToUnstake(address to, address unauthorizedUser, uint256 tokenId) public {
+        vm.assume(to != address(0));
+        vm.assume(unauthorizedUser != address(0));
+        vm.assume(to != unauthorizedUser);
+        vm.assume(to != address(tokenMock));
+
+        vm.startPrank(to);
+        wrappedTokenMock.mint(to, tokenId);
+        wrappedTokenMock.setApprovalForAll(address(tokenMock), true);
+        tokenMock.stake(tokenId);
+        vm.stopPrank();
+
+        vm.startPrank(unauthorizedUser);
+        vm.expectRevert(ERC721CW.ERC721CW__CallerNotOwnerOfWrappingToken.selector);
+        tokenMock.unstake(tokenId);
+        vm.stopPrank();
+    }
+
+    function testRevertsWhenApprovedOperatorAttemptsToUnstake(address to, address approvedOperator, uint256 tokenId) public {
+        vm.assume(to != address(0));
+        vm.assume(approvedOperator != address(0));
+        vm.assume(to != approvedOperator);
+        vm.assume(to != address(tokenMock));
+
+        vm.startPrank(to);
+        wrappedTokenMock.mint(to, tokenId);
+        wrappedTokenMock.setApprovalForAll(address(tokenMock), true);
+        wrappedTokenMock.setApprovalForAll(approvedOperator, true);
+        tokenMock.setApprovalForAll(approvedOperator, true);
+        tokenMock.stake(tokenId);
+        vm.stopPrank();
+
+        vm.startPrank(approvedOperator);
+        vm.expectRevert(ERC721CW.ERC721CW__CallerNotOwnerOfWrappingToken.selector);
+        tokenMock.unstake(tokenId);
+        vm.stopPrank();
+    }
+
+    function testRevertsWhenUserAttemptsToUnstakeATokenThatHasNotBeenStaked(address to, uint256 tokenId) public {
+        vm.assume(to != address(0));
+        vm.assume(to != address(tokenMock));
+
+        vm.startPrank(to);
+        wrappedTokenMock.mint(to, tokenId);
+        wrappedTokenMock.setApprovalForAll(address(tokenMock), true);
+        vm.expectRevert("ERC721: invalid token ID");
+        tokenMock.unstake(tokenId);
+        vm.stopPrank();
+    }
+
+    function testWrappingCollectionHoldersCannotUnstakeTokensBeforeTimeLockExpires(address to, uint256 tokenId) public {
+        vm.assume(to != address(0));
+        vm.assume(to != address(tokenMock));
+
+        vm.startPrank(to);
+        wrappedTokenMock.mint(to, tokenId);
+        wrappedTokenMock.setApprovalForAll(address(tokenMock), true);
+        tokenMock.stake(tokenId);
+        uint256 expirationTimestamp = tokenMock.getStakedTimestamp(tokenId) + tokenMock.getTimelockSeconds();
+        vm.warp(expirationTimestamp - 1);
+        vm.expectRevert(ERC721CWTimeLockedUnstake.ERC721CWTimeLockedUnstake__TimelockHasNotExpired.selector);
+        tokenMock.unstake(tokenId);
+        vm.stopPrank();
+    }
+
+    function testWrappingCollectionHoldersCanUnstakeTokensIfAtExactTimelockExpiration(address to, uint256 tokenId) public {
+        vm.assume(to != address(0));
+        vm.assume(to != address(tokenMock));
+
+        vm.startPrank(to);
+        wrappedTokenMock.mint(to, tokenId);
+        wrappedTokenMock.setApprovalForAll(address(tokenMock), true);
+        tokenMock.stake(tokenId);
+        uint256 expirationTimestamp = tokenMock.getStakedTimestamp(tokenId) + tokenMock.getTimelockSeconds();
+        vm.warp(expirationTimestamp);
+        tokenMock.unstake(tokenId);
+        vm.stopPrank();
+
+        vm.expectRevert("ERC721: invalid token ID");
+        address ownerOfWrapper = tokenMock.ownerOf(tokenId);
+        assertEq(wrappedTokenMock.ownerOf(tokenId), to);
+    }
+
+    function testWrappingCollectionHoldersCanUnstakeTokensAnytimeAfterTimelockExpiration(address to, uint256 tokenId, uint256 secondsPastExpiration) public {
+        vm.assume(to != address(0));
+        vm.assume(to != address(tokenMock));
+
+        vm.startPrank(to);
+        wrappedTokenMock.mint(to, tokenId);
+        wrappedTokenMock.setApprovalForAll(address(tokenMock), true);
+        tokenMock.stake(tokenId);
+        uint256 expirationTimestamp = tokenMock.getStakedTimestamp(tokenId) + tokenMock.getTimelockSeconds();
+        uint256 maxSecondsPast = type(uint256).max - expirationTimestamp;
+        vm.assume(secondsPastExpiration < maxSecondsPast);
+        vm.warp(expirationTimestamp + secondsPastExpiration);
+        tokenMock.unstake(tokenId);
+        vm.stopPrank();
+
+        vm.expectRevert("ERC721: invalid token ID");
+        address ownerOfWrapper = tokenMock.ownerOf(tokenId);
+        assertEq(wrappedTokenMock.ownerOf(tokenId), to);
+    }
+
+    function testSecondaryWrappingCollectionHoldersCanUnstakeTokensAnytimeAfterTimelockExpiration(address to, address secondaryHolder, uint256 tokenId, uint256 secondsPastExpiration) public {
+        vm.assume(to != address(0));
+        vm.assume(to != address(tokenMock));
+        vm.assume(secondaryHolder != address(0));
+        vm.assume(secondaryHolder != address(tokenMock));
+        vm.assume(to != secondaryHolder);
+
+        vm.startPrank(to);
+        wrappedTokenMock.mint(to, tokenId);
+        wrappedTokenMock.setApprovalForAll(address(tokenMock), true);
+        tokenMock.stake(tokenId);
+        tokenMock.transferFrom(to, secondaryHolder, tokenId);
+        vm.stopPrank();
+
+        vm.startPrank(secondaryHolder);
+        uint256 expirationTimestamp = tokenMock.getStakedTimestamp(tokenId) + tokenMock.getTimelockSeconds();
+        uint256 maxSecondsPast = type(uint256).max - expirationTimestamp;
+        vm.assume(secondsPastExpiration < maxSecondsPast);
+        vm.warp(expirationTimestamp + secondsPastExpiration);
+        tokenMock.unstake(tokenId);
+        vm.stopPrank();
+
+        vm.expectRevert("ERC721: invalid token ID");
+        address ownerOfWrapper = tokenMock.ownerOf(tokenId);
+        assertEq(wrappedTokenMock.ownerOf(tokenId), secondaryHolder);
+    }
+
+    function testRevertsWhenNativeFundsIncludedInUnstake(address to, uint256 tokenId, uint256 secondsPastExpiration, uint256 value) public {
+        vm.assume(to != address(0));
+        vm.assume(to != address(tokenMock));
+        vm.assume(value > 0);
+
+        vm.deal(to, value);
+
+        vm.startPrank(to);
+        wrappedTokenMock.mint(to, tokenId);
+        wrappedTokenMock.setApprovalForAll(address(tokenMock), true);
+        tokenMock.stake(tokenId);
+        uint256 expirationTimestamp = tokenMock.getStakedTimestamp(tokenId) + tokenMock.getTimelockSeconds();
+        uint256 maxSecondsPast = type(uint256).max - expirationTimestamp;
+        vm.assume(secondsPastExpiration < maxSecondsPast);
+        vm.warp(expirationTimestamp + secondsPastExpiration);
+        vm.expectRevert(ERC721CW.ERC721CW__DefaultImplementationOfUnstakeDoesNotAcceptPayment.selector);
+        tokenMock.unstake{value: value}(tokenId);
+        vm.stopPrank();
+    }
+}
