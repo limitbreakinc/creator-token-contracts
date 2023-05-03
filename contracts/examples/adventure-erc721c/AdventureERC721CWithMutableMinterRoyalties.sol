a0f5e62d79f1431c5e61e16923fd279ed4ce869d
Nathan Gang <nathan.gang@limitbreak.com>
2023-05-02 18:36:33 -0600

    Add AdventureERC721C examples

---
+++ contracts/examples/adventure-erc721c/AdventureERC721CWithMutableMinterRoyalties.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "../../erc721c/AdventureERC721C.sol";
+import "../../programmable-royalties/MutableMinterRoyalties.sol";
+
+contract ERC721CWithMutableMinterRoyalties is AdventureERC721C, MutableMinterRoyalties {
+
+    constructor(
+        uint96 defaultRoyaltyFeeNumerator_,
+        uint256 maxSimultaneousQuests_,
+        string memory name_,
+        string memory symbol_) 
+        AdventureERC721C(maxSimultaneousQuests_, name_, symbol_) 
+        MutableMinterRoyalties(defaultRoyaltyFeeNumerator_) {
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(AdventureERC721C, MutableMinterRoyalties) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function mint(address to, uint256 tokenId) external {
+        _mint(to, tokenId);
+    }
+
+    function safeMint(address to, uint256 tokenId) external {
+        _safeMint(to, tokenId);
+    }
+
+    function burn(uint256 tokenId) external {
+        _burn(tokenId);
+    }
+
+    function _mint(address to, uint256 tokenId) internal virtual override {
+        _onMinted(to, tokenId);
+        super._mint(to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal virtual override {
+        super._burn(tokenId);
+        _onBurned(tokenId);
+    }
+}
